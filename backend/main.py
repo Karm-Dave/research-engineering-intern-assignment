2253b279744b4007907a59b9fb3ff09552729587
Karm <karmd@example.com>
2026-04-06 09:49:18 +0530

    Backend connection testing

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI(title="SimPPL Research Dashboard")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://arcanumdata.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
